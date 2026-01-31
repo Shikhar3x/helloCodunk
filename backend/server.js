09c0d4142b0c2f338da83b7dac810160b9ae2175
Shikhar <shikharx.dev@gmail.com>
2026-01-31 23:07:58 +0530

    added something

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import codeRoutes from "./routes/codeRoutes.js";
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
@@ -13,16 +14,15 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST"],
-  }),
+  })
 );
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Codunk API running");
-});
-
 app.use("/api/code", codeRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
