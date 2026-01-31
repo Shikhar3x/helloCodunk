69cb648c6c257405b2e66cbd10edfcce12acabc5
Shikhar <shikharx.dev@gmail.com>
2026-01-31 23:29:48 +0530

    add final

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import connectDB from "./config/db.js";
 import codeRoutes from "./routes/codeRoutes.js";
 
@@ -9,23 +12,25 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Codunk backend running");
-});
-
+// API
 app.use("/api/code", codeRoutes);
 
-const PORT = process.env.PORT || 10000;
+// ===== STATIC FRONTEND =====
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/dist/index.html")
+  );
 });
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () =>
+  console.log("Server running on port", PORT)
+);
